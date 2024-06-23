3ea5a0c14fd6a0c2b2d31e9b19e0ce1a4ba43608
azizurrehman2430 <azizurrehman2430@gmail.com>
2024-06-23 16:35:47 +0500

    portfolio done

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// var typed = new typed(".text",{
+//     strings:["Web Developer" , "Graphic Designer" ,"Programmer"],
+//     typeSpeed: 100,
+//     backSpeed: 100,
+//     backDelay: 1000,
+//     loop: true
+// });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var typed = new Typed('.text', {
+        strings: ["Web Developer", "Graphic Designer", "Programmer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+}); 
